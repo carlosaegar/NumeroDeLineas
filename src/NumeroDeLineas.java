6967d0ec28ea106483493b518f627cd81c097de3
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-17 14:00:57 +0100

    Update NumeroDeLineas.java

--- src/NumeroDeLineas.java
+++ src/NumeroDeLineas.java
@@ -1,13 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.util.Scanner;
+import java.io.IOException;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+
+public class NumeroDeLineas {
+    public static void main(String[] args) {
+        File archivo = new File("datos.txt");
+        int contador = 0;
+
+        try (Scanner lector = new Scanner(archivo)) {
+
+            while (lector.hasNextLine()) {
+                lector.nextLine(); // Leemos la línea actual
+                contador++;        // Sumamos 1 al contador por cada línea encontrada
+            }
+
+            System.out.println("El archivo '" + archivo.getName() + "' tiene " + contador + " líneas.");
+
+        }  catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+
+        }
+    }
 }
